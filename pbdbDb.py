9f581fc2e417961c4d69d714ff8cfcc4b6d88841
Aaron Bradley <arbrad@gmail.com>
2017-09-03 14:19:36 -0600

    Bug fixes + non-binned species/time analysis
    Fixed bugs related to binning.
    
    Binning seems to have too many artifacts for the species/time analysis, so I switched by default to fixed 1 my intervals.

--- pbdbDb.py
+++ pbdbDb.py
@@ -19,7 +19,7 @@ from sklearn.linear_model import LinearRegression
 class DB:
     '''Holds and analyzes PBDB dataset.'''
     
-    # geneeral setup
+    # general setup
     # resource directory
     rd = 'resources/'
     # fields to exclude from dataset
@@ -64,9 +64,9 @@ class DB:
             self.range[sp] = (max(ce,e), min(cl,l))
         # time bins by species
         self.bins = {}
-        for sp,(e,l) in self.range.items():
+        for sp, (e,l) in self.range.items():
             eb = bisect.bisect_right(self.timeSplits, e)
-            lb = bisect.bisect_left(self.timeSplits, e)
+            lb = bisect.bisect_left(self.timeSplits, l)
             assert lb <= eb
             self.bins[sp] = list(range(lb,eb+1))
     
@@ -76,10 +76,10 @@ class DB:
         def f(r):
             try:
                 lat, lng = float(r[latstr]), float(r[lngstr])
-                if lat > 90: lat -= 180
-                if lat <= -90: lat += 180
-                if lng > 90: lng -= 180
-                if lng <= -90: lng += 180
+                if lat > 180: lat -= 360
+                if lat <= -180: lat += 360
+                if lng > 180: lng -= 360
+                if lng <= -180: lng += 360
                 mlat, mlng = math.ceil(lat/degrees), math.ceil(lng/degrees)
                 return (mlat, mlng)
             except:
@@ -99,14 +99,22 @@ class DB:
                         taxaName=self.taxaName, data=db)
         return dbs
 
+    def species(self):
+        '''Iterator over species in dataset.'''
+        for sp in self.range:
+            yield sp
+    def fossilRange(self, species):
+        '''Fossil range of species.'''
+        return self.range[species]
+
     def period(self, binId):
-        '''Return range corresponding to the bin ID.'''
+        '''Range corresponding to bin ID.'''
         e = 4500 if binId == len(self.timeSplits) else self.timeSplits[binId]
         l = 0 if binId == 0 else self.timeSplits[binId-1]
         return (e, l)
 
     def speciesByTime(self):
-        '''Return a list of species occurring in each time bin.'''
+        '''List of species occurring in each time bin.'''
         bins = [[] for _ in range(len(self.timeSplits)+1)]
         for sp, binIds in self.bins.items():
             for binId in binIds:
@@ -247,7 +255,7 @@ def testPCA():
     db.plot([0,1,2], db.colorByTime(), db.fieldSubset('order','Scleractinia'))
     return db
 
-def speciesOverTime(db, degrees=None, modifier=''):
+def speciesOverTime(db, degrees=None, modifier='', byBin=False, cutoffThresh=.05):
     '''Construct a plot of species-over-time by region of the world.
     Set modifer='paleo' to use paleocoordinates instead of modern
     coordinates. By default, the world is treated as one region.'''
@@ -260,7 +268,7 @@ def speciesOverTime(db, degrees=None, modifier=''):
         del dbs['No location']
     keys = set(dbs.keys())
     if degrees:
-        loci = list(range(math.ceil(-89/degrees), math.ceil(90/degrees)+1))
+        loci = list(range(math.ceil(-179/degrees), math.ceil(180/degrees)+1))
         for x in loci:
             for y in loci:
                 k = (x, y)
@@ -274,9 +282,26 @@ def speciesOverTime(db, degrees=None, modifier=''):
         if loc not in dbs: continue
         db = dbs[loc]
         ax = fig.add_subplot(n, n, i+1)
-        z = db.speciesByTime()
-        x = [-db.period(i)[1] for i in range(len(z))]
-        y = [len(s) for s in z]
+        if byBin:
+            # count by time bins, but normalize to species/my
+            z = db.speciesByTime()
+            x = [-db.period(i)[1] for i in range(len(z))]
+            # normalize to species/my, with a minimum of 1 my per period
+            y = [len(s)/max(1,db.period(i)[0]-db.period(i)[1]) for 
+                 i, s in enumerate(z)]
+        else:
+            # use fossil ranges explicitly, by my
+            x = [-my for my in range(2000)]
+            y = [0 for _ in x]
+            for sp in db.species():
+                e, l = db.fossilRange(sp)
+                for i in range(math.floor(l), math.floor(e)):
+                    y[i] += 1
+            # cut graph to to start at first significant time
+            thresh = cutoffThresh * max(y)
+            fnz = len(y)-1
+            while y[fnz] < thresh: fnz -= 1
+            x, y = x[:fnz+1], y[:fnz+1]
         ax.plot(x, y, '.')
         lr = LinearRegression()
         fz = 0
@@ -285,8 +310,16 @@ def speciesOverTime(db, degrees=None, modifier=''):
                 fz = i+1
                 break
         x, y = x[:fz], y[:fz]
-        lr.fit(np.array([[v] for v in x]), np.array(y))
-        a, b = lr.coef_, lr.intercept_
-        ax.plot([x[0],x[-1]], [b,b+a*x[-1]])
+        if len(x) > 1:
+            weights = [1 for _ in x]
+            if byBin:
+                # weight by bin period, taking care of final long one
+                periods = [db.period(i)[0]-db.period(i)[1] for i in range(len(y))]
+                maxW = max(periods[:-1])
+                periods[-1] = min(maxW, periods[-1])
+                weights = periods
+            lr.fit(np.array([[v] for v in x]), np.array(y), weights)
+            a, b = lr.coef_, lr.intercept_
+            ax.plot([x[0],x[-1]], [b,b+a*x[-1]])
         ax.set_title(str(loc))
         
