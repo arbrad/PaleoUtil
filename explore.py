dbec7807f84c2fb2ea85d4232ae6c55a008b594a
Aaron Bradley <arbrad@gmail.com>
2017-09-04 23:26:12 -0600

    Slightly less naive affinity
    Computing the multinomial using logs to avoid big numbers. Adding marginalizing over taxa and H1 frequency. Added a difference distribution.

--- explore.py
+++ explore.py
@@ -82,28 +82,70 @@ def timeAnalysis(db, start=300):
     db.plot([0,1], db.colorByTime(start=start), species=scler)
     db.plotMeanPCAOverTime([0,1], species=scler, start=start)
 
-def ncr(N, Rs_):
-    Rs = Rs_[:]
-    Rs.sort()
-    numer = reduce(op.mul, range(N, Rs[-1], -1), 1)
-    denom = reduce(op.mul, (reduce(op.mul, range(1, x+1), 1) for x in Rs[:-1]))
-    return numer//denom
+lncr_ = {}
+def lncr(N, Rs):
+    key = str(N) + ':' + ','.join(str(x) for x in Rs)
+    if key not in lncr_:
+        num = sum(math.log(x) for x in range(N, Rs[-1], -1))
+        den = sum(sum(math.log(x) for x in range(1, x+1)) for x in Rs[:-1])
+        lncr_[key] = num-den
+    return lncr_[key]
+def lmultinomial(N, Rs, Ps):
+    return lncr(N, Rs) + sum(Rs[i]*math.log(Ps[i]) for i in range(len(Ps)))
 def multinomial(N, Rs, Ps):
-    x, y = sum(Rs), sum(Ps)
+    assert sum(Rs) == N
     assert len(Rs) == len(Ps)
-    return ncr(N, Rs) * reduce(op.mul, (Ps[i]**Rs[i] for i in range(len(Ps))))
-def p(dist=[0,25,1,99]):
+    for i in range(len(Rs)):
+        if not Ps[i]:
+            if Rs[i]: return 0
+            return multinomial(N, Rs[:i]+Rs[i+1:], Ps[:i]+Ps[i+1:])
+    return math.exp(lmultinomial(N, Rs, Ps))
+
+def pAffinity(dist=[0,250,0,1000], grid=100, margin=False, plot=True):
+    '''For dist=[H1-taxa-fossil, H1-other, H2-taxa-fossil, H2-other],
+    compute the probability distribution of affinity for H1. When
+    margin=False, the computation uses the sample frequency of taxa
+    fossils, f, and H1 fossils, h. When margin=True, the distribution 
+    is computed by marginalizing out f and h from a joint a-f-h
+    distribution. grid controls the mesh granularity.'''
     N = sum(dist)
-    f = (dist[0]+dist[2])/N
-    h = sum(dist[:2])/N
-    def g(a):
+    def g(a,f,h):
+        # a = affinity for H1
+        # f = frequency of taxa of interest
+        # h = frequency of H1 samples compared to H2
         return multinomial(N, dist,
                            [a*f*h,
                             (1-a*f)*h,
                             (1-a)*f*(1-h),
                             (1-(1-a)*f)*(1-h)])
-    x = [g(a) for a in np.linspace(0,1,100)]
+    if margin:
+        # Naive mesh, will switch to MCMC
+        mesh = np.linspace(0,1,grid+1)
+        x = [sum(g(a,f,h) for f in mesh for h in mesh) for a in mesh]
+    else:
+        # Use sample means for f, h
+        f = (dist[0]+dist[2])/N
+        h = sum(dist[:2])/N
+        x = [g(a,f,h) for a in np.linspace(0,1,grid+1)]
+    unit = 1/grid
     s = sum(x)
-    x = [e/s for e in x]
-    plt.plot(np.linspace(0,1,100), x)
-    print('%.2f %.2f'%(sum(x[:10]),sum(x[90:])))
+    x = [e/s/unit for e in x]
+    if plot:
+        plt.plot(np.linspace(0,1,len(x)), x)
+        tenth = math.floor(0.1*(len(x)-1))
+        print('%.2f %.2f'%(unit*sum(x[:1+tenth]),unit*sum(x[-tenth-1:])))
+    return x
+
+def pAffinityChange(dist0, dist1, grid=100, margin=False):
+    '''For two distributions as in pAffinity, cmpute the 
+    probability distribution of the change in affinity for H1.'''
+    p0 = pAffinity(dist0, grid, margin, plot=True)
+    p1 = pAffinity(dist1, grid, margin, plot=True)
+    x = [0 for _ in range(2*len(p0)-1)]
+    mesh = np.linspace(0,1,grid+1)
+    for i, a0 in enumerate(mesh):
+        for j, a1 in enumerate(mesh):
+            d = a1-a0
+            x[int((d+1)*grid+0.5)] += p0[i]*p1[j]/grid
+    plt.plot(np.linspace(-1,1,2*grid+1), x)
+    print([d[0]/d[1]/(d[0]/d[1]+d[2]/d[3]) for d in [dist0,dist1]])
